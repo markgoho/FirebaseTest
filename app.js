b067f2f13eb20b30c7bca2abbb116cbb8ffe6e40
Mark Goho <markgoho@gmail.com>
2017-01-03 17:16:46 -0500

    List synchronization
    Adding methods to update list items when DB data is changed

--- app.js
+++ app.js
@@ -8,9 +8,33 @@ const config = {
   firebase.initializeApp(config);
 
   const preObject = document.getElementById('object');
+  const ulList = document.getElementById('list');
+
   const dbRefObject = firebase.database().ref().child('object');
+  const dbRefList = dbRefObject.child('hobbies');
 
   // Sync object changes
   dbRefObject.on('value', snap => {
     preObject.innerText = JSON.stringify(snap.val(), null, 3);
+  });
+
+  // Sync list changes
+  // child_added is list synchronization
+  dbRefList.on('child_added', snap => {
+    const li = document.createElement('li');
+    li.innerText = snap.val();
+    li.id = snap.key; // key name for each item
+    ulList.appendChild(li);
+  });
+
+  // Update items when they are changed
+  dbRefList.on('child_changed', snap => {
+    const liChanged = document.getElementById(snap.key);
+    liChanged.innerText = snap.val();
+  });
+
+  // Update items when they are removed
+  dbRefList.on('child_removed', snap => {
+    const liToRemove = document.getElementById(snap.key);
+    liToRemove.remove();
   });
